c1aff925756ede205700b9c2fbb722aff0e01983
yankui <275561618@qq.com>
2018-08-20 18:42:43 +0800

    Added for Performance-> employee pages

--- src/main/java/com/pmo/dashboard/controller/PerformanceController.java
+++ src/main/java/com/pmo/dashboard/controller/PerformanceController.java
@@ -1,26 +1,37 @@
 package com.pmo.dashboard.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pmo.dashboard.entity.User;
 
 @Controller
 @RequestMapping(value="/performance")
 public class PerformanceController {
 	
+	private static Logger logger = LoggerFactory.getLogger(PerformanceController.class);
 	
 	@SuppressWarnings("unused")
 	private ObjectMapper objectMapper = new ObjectMapper();
 	
 	
-	@RequestMapping("/performancemain")
-	public String listPage(HttpServletRequest request,Model model){
-		
-		return "performance/performance";
+	@RequestMapping("/listPage")
+	public String listPage(HttpServletRequest request,Model model){		
+		return "performance/performanceListPage";
 	}
+
+
+	@RequestMapping("/performanceEmpPBC")
+	public String getTMemployee(final HttpServletRequest request, Model model){
+		return "performance/performanceEmpPBC";
+	}
+	
 
 }
---
+++ src/main/webapp/js/pmo/performance.js
@@ -0,0 +1,14 @@
+$("#getEmp").click(function(){
+	window.open(path+"/service/performance/performanceEmpPBC.html?status=1");
+} );
+
+$("#getManage").click(function(){
+	window.open(path+"/service/employee/getTMemployee.html?status=3");
+});
+$("#getHR").click(function(){
+	window.open(path+"/service/employee/getTMemployee.html?status=4");
+});
+$("#getLOB").click(function(){
+	window.open(path+"/service/employee/getTMemployee.html?status=4");
+});
+
